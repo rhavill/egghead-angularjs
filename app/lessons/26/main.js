2e0968f0f46153f493acb0cf8db74663de98fb32
Ricardo Havill <rhavill@progresstesting.com>
2013-08-01 06:18:01 -0400

    Added some notes and restored code to earlier state.

--- app/lessons/26/main.js
+++ app/lessons/26/main.js
@@ -1,17 +1,21 @@
 var app = angular.module("app", []);
 
+// When the 2nd parameter passed to app.controller is a function, the parameters passed to that function must match an existing provider (dependency injection).
+//app.controller("MyCtrl", function ($scope, $http, $parse) {
+
+//});
+
+// Another way to set up dependency injection in a controller is to pass an array as the 2nd paramter to app.controller. This protects against JS minification.
 app.controller("MyCtrl", ['$scope','$http', function ($scope,$http) {
   console.log($scope);
 }]);
 
-app.controller("MyCtrl", function ($scope, $http, $parse) {
-
-});
-
 app.directive("myDirective", function ($http,$parse) {
   return {
+  	//scope: {}, Isolating the scope gives this function access to a different scope from MyCtrl
     link: function(scope, element, attrs) {
-      console.log(element);
+      // The parameter names in the function do not matter. The order of the parameters determines what gets passed to the function.
+      console.log(scope);
     }
   };
 });
